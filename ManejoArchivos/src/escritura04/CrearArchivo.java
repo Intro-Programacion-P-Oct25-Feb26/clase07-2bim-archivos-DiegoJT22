fa71ed6920d80004c8b6dbd535bb4ee5f5392a28
DiegoJT22 <daigoestrlar22@gmail.com>
2026-01-29 11:34:32 -0500

    Solucion paquete escritura04

---
+++ ManejoArchivos/src/escritura04/CrearArchivo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package escritura04;
+
+import java.util.Formatter;
+
+public class CrearArchivo {
+
+    public static void agregarRegistros(String valor, String nombreArchivo) {
+        String ruta = String.format("data/%s.txt", nombreArchivo);
+
+        try {
+            Formatter salida = new Formatter(ruta);
+            salida.format("%s", valor);
+            salida.close();
+        } catch (Exception e) {
+            System.err.println("Error al crear archivo");
+            System.exit(1);
+
+        }
+    }
+}
---
+++ ManejoArchivos/src/escritura04/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package escritura04;
+
+import escritura03.CrearArchivoTexto;
+import java.util.Scanner;
+
+/**
+ *
+ * @author daigo
+ */
+public class Main {
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        String cadenaFinal = "";
+        String cedula = "";
+
+        for (int i = 1; i < 3; i++) {
+
+            System.out.println("Ingrese cedula");
+            cedula = entrada.nextLine();
+            System.out.println("Ingrese nombre");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingrese correo");
+            String correo = entrada.nextLine();
+
+            cadenaFinal = String.format("%s %s %s\n", cedula,nombre,correo);
+            CrearArchivo.agregarRegistros(cadenaFinal,cedula);
+        }
+
+    } 
+}
